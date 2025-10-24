fed9f4eb292d39671cfea78e9919926a0d40c6bf
Viliyan Raychov <114240708+Viliyancho@users.noreply.github.com>
2025-10-24 23:58:56 +0300

    Feature/multiply button (#4)
    * Change 1
    
    * Change 2
    
    * Change 3

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function add(a, b) {
   return a + b;
 }
-
+//Change 1
 function subtract(a, b) {
   return a - b;
 }
-
+//Change 2
 document.getElementById('btn-add').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = add(a, b);
 });
-
+//Change 3
 document.getElementById('btn-subtract').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
